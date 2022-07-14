126c24a568c7e5e90ff3e40916a71fe4db28bb0f
Gabriel <gabrielcoelhodev1995@gmail.com>
2022-07-14 15:27:36 -0300

    atualização do linit

--- script.js
+++ script.js
@@ -1,60 +1,36 @@
 const login = document.getElementById('btnLogin');
-
-function loginInfo(){
-    let user = document.getElementById('login').value
-    let password = document.getElementById('password').value
-    if(user === 'tryber@teste.com' && password === '123456')
-    alert('Olá, Tryber!');
-    else {
-        alert('Email ou senha inválidos.')
-        console.log('oiiii');
-    };
-};
+function loginInfo() {
+  const user = document.getElementById('login').value;
+  const password = document.getElementById('password').value;
+  if (user === 'tryber@teste.com' && password === '123456') alert('Olá, Tryber!');
+  else {
+    alert('Email ou senha inválidos.');
+  }
+}
 login.addEventListener('click', loginInfo);
-
-
-
-
-
+const subBtn = document.querySelector('#submit-btn');
 function verificaCheckBox() {
-  if(document.getElementById('agreement').checked === false){
-    return document.querySelector('#submit-btn').disabled = true
-  }else {
-    return document.querySelector('#submit-btn').disabled = false
-
-  }
-    
-
-}
-verificaCheckBox()
-
-
-let pegaCheck = document.getElementById('agreement');
-function habilitaCheckBox(){
-  if(document.getElementById('agreement').checked === false) {
-    document.querySelector('#submit-btn').disabled = true
-  }else if(document.getElementById('agreement').checked === true) {
-    document.querySelector('#submit-btn').disabled = false
+  const desabilita = subBtn;
+  const habilita = subBtn;
+  if (document.getElementById('agreement').checked === false) {
+    habilita.disabled = true;
+  } else {
+    desabilita.disabled = false;
   }
 }
-
-
-
-pegaCheck.addEventListener('click', habilitaCheckBox)
-
-
-
-
-let form = document.querySelector('.conteiner');
-form.addEventListener('submit', function(event) {
+verificaCheckBox();
+const pegaCheck = document.getElementById('agreement');
+function habilitaCheckBox() {
+  if (document.getElementById('agreement').checked === false) {
+    subBtn.disabled = true;
+  } else if (document.getElementById('agreement').checked === true) {
+    subBtn.disabled = false;
+  }
+}
+pegaCheck.addEventListener('click', habilitaCheckBox);
+const form = document.querySelector('.conteiner');
+form.addEventListener('submit', (event) => {
   if (document.getElementById('agreement').checked === false) {
     event.preventDefault();
   }
-})
-
-
-
-
-// const botao = document.querySelector('#submit-btn').disabled = true
-
-
+});
