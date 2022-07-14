10400f23c70086f230e0b4315ce0a0a6e55b27b6
Gabriel <gabrielcoelhodev1995@gmail.com>
2022-07-14 14:13:30 -0300

    requisitos concluidos até o 19

--- script.js
+++ script.js
@@ -12,3 +12,49 @@ function loginInfo(){
 };
 login.addEventListener('click', loginInfo);
 
+
+
+
+
+function verificaCheckBox() {
+  if(document.getElementById('agreement').checked === false){
+    return document.querySelector('#submit-btn').disabled = true
+  }else {
+    return document.querySelector('#submit-btn').disabled = false
+
+  }
+    
+
+}
+verificaCheckBox()
+
+
+let pegaCheck = document.getElementById('agreement');
+function habilitaCheckBox(){
+  if(document.getElementById('agreement').checked === false) {
+    document.querySelector('#submit-btn').disabled = true
+  }else if(document.getElementById('agreement').checked === true) {
+    document.querySelector('#submit-btn').disabled = false
+  }
+}
+
+
+
+pegaCheck.addEventListener('click', habilitaCheckBox)
+
+
+
+
+let form = document.querySelector('.conteiner');
+form.addEventListener('submit', function(event) {
+  if (document.getElementById('agreement').checked === false) {
+    event.preventDefault();
+  }
+})
+
+
+
+
+// const botao = document.querySelector('#submit-btn').disabled = true
+
+
